94acdc0acd77fb1983c10232f03d24d319301b2b
flylive302 <sarasobania01@gmail.com>
2025-10-16 16:19:05 +0500

    Starting Project FlyLive Frontend - Basic Nuxt Ui Setup Done

---
+++ app/pages/(home)/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <h1 class="text-purple-700 text-3xl">Hello Fly Live</h1>
+    <UButton color="primary">Save Changes</UButton>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
