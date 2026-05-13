868f555b5df39b64e8aa1e4c3897fc89dd9bf097
flylive302 <sarasobania01@gmail.com>
2026-05-13 08:17:51 +0500

    feat: redesign auth layout with decorative assets and update social login UI to full-width buttons

--- app/layouts/auth.vue
+++ app/layouts/auth.vue
@@ -7,10 +7,19 @@ const authHeading = computed(() => route.meta.authHeading as string | undefined)
 </script>
 
 <template>
-  <main class="px-4 mt-16">
-    <h1 class="text-3xl font-bold text-center w-60 mb-4 mx-auto">{{authHeading}}</h1>
-    <slot />
-    <USeparator color="primary" class="my-4" label="OR" />
-    <AuthSocialAuth class="w-full" />
+  <main class="relative h-screen w-screen overflow-x-hidden pt-16">
+    <div class="absolute z-0 -top-30 left-1/2 -translate-x-1/2 blur-3xl bg-primary/20 rounded-full size-64 animate-pulse will-change-[opacity]"></div>
+    <h1 class="text-3xl font-bold text-center w-60 mx-auto">{{authHeading}}</h1>
+
+    <div class="flex justify-between w-full mx-auto  relative z-10 pointer-events-none select-none" aria-hidden="true">
+      <img src="/images/auth-deco.png" width="180" height="auto" class="-rotate-9 scale-x-[-1]" loading="lazy" fetchpriority="low" decoding="async" alt="">
+      <img src="/images/auth-deco.png" width="180" height="auto" class="rotate-9" loading="lazy" fetchpriority="low" decoding="async" alt="">
+    </div>
+
+    <div class="p-6 relative z-10 backdrop-blur-sm -mt-18 min-h-full">
+      <slot />
+      <USeparator color="primary" class="my-4" label="OR" />
+      <AuthSocialAuth class="w-full" />
+    </div>
   </main>
 </template>
